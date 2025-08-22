578936656725511c757ef34532a55a8ea12755f7
martru118 <50148537+martru118@users.noreply.github.com>
2025-08-22 00:54:55 -0400

    Tutorial: store user data in db

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,8 +4,11 @@ import { Button } from "./ui/button";
 import { PenBox } from "lucide-react";
 import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
 import UserMenu from "./UserMenu";
+import { checkUser } from "@/lib/checkUser";
 
-function Navbar() {
+async function Navbar() {
+  await checkUser()
+  
   return (
     <nav className="mx-auto py-2 px-4 flex justify-between items-center shadow-md border-b-2">
       <Link href={"/"} className="flex items-center">
---
+++ lib/checkUser.ts
@@ -0,0 +1,37 @@
+import { clerkClient, currentUser } from "@clerk/nextjs/server";
+import db from "./prisma";
+
+export async function checkUser() {
+  const user = await currentUser()
+  if (!user) return null
+
+  try {
+    // user is logged in
+    const loggedInUser = await db?.user.findUnique({
+      where: {
+        clerkUserId: user.id,
+      }
+    })
+    if (loggedInUser) return loggedInUser
+
+    // generate dummy username based on slug
+    const name = `${user.firstName} ${user.lastName}`;
+    const slug = name.split(" ").join("-") + user.id.slice(-4);
+    (await clerkClient()).users.updateUser(user.id, {
+      username: slug
+    })
+
+    // add new user to database
+    const newUser = await db.user.create({
+      data: {
+        clerkUserId: user.id,
+        name,
+        imageUrl: user.imageUrl,
+        email: user.emailAddresses[0].emailAddress,
+        username: slug,
+      }
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
